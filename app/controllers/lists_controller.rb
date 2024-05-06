f51f6ce93aaee73f0a10ed9b5c1f4a1faea882bb
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-05-06 01:33:06 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,51 @@
+class ListsController < ApplicationController
+ 
+  def new
+    @list = List.new
+  end 
+  
+  def create
+    # １. データを受け取り新規登録するためのインスタンス作成
+    @list = List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    if @list.save
+      # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
+    else
+      flash.now[:alert]="投稿に失敗しました。"
+      render :new
+    end
+  end
+  
+  def index
+    @lists = List.all
+  end
+  
+  def show
+    @list = List.find(params[:id])
+  end 
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+  
+  def destroy
+    list = List.find(params[:id])  
+    list.destroy  
+    redirect_to '/lists'  
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :im)
+  end
+end 
+  
